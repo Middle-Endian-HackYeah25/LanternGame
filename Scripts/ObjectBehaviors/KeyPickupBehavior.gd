1103595c435a01a0726e0854e00772aa6ee52f03
Michał Szyc <181160412+michau-s@users.noreply.github.com>
2025-10-05 01:09:45 +0200

    Added key pickup sound

--- Scripts/ObjectBehaviors/KeyPickupBehavior.gd
+++ Scripts/ObjectBehaviors/KeyPickupBehavior.gd
@@ -6,13 +6,15 @@ func _on_body_entered(body: Node) -> void:
 	if not body.is_in_group("player"):
 		return
 	Inventory.add_key(1)
+	var s = AudioStreamPlayer2D.new()
 	if pickup_sound:
-		var s = AudioStreamPlayer2D.new()
+		add_sibling(s)
 		s.stream = pickup_sound
-		add_child(s)
 		s.play()
 	#delete instance
 	queue_free()
+	await s.finished
+	s.free()
 
 func _ready() -> void:
 	connect("body_entered", Callable(self, "_on_body_entered"))
