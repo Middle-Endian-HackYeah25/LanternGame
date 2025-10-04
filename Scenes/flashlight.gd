ccb70495c20485857ba6cafddff8668cc4538d41
MateuszBlazejowski <matblaz12345@gmail.com>
2025-10-04 20:20:52 +0200

    script basicenemy

---
+++ Scenes/flashlight.gd
@@ -0,0 +1,12 @@
+extends PointLight2D
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("LightSensitive"):
+		body._entered_light();
+		print("InLight")
+		
+func _on_area_2d_body_exited(body):
+	if body.is_in_group("LightSensitive"):
+		body._left_light();
+		print("NotInLight")
