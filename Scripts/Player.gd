26bfee66ec561a5929e29bb45c1402b4335ba874
Michał Szyc <181160412+michau-s@users.noreply.github.com>
2025-10-04 15:33:36 +0200

    Added movable box

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+const PUSH_FORCE = 10.0
 
 func _physics_process(delta):
 	
@@ -10,5 +11,10 @@ func _physics_process(delta):
 		velocity = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0 , SPEED)
-		velocity.x = move_toward(velocity.x, 0 , SPEED)
+		velocity.y = move_toward(velocity.y, 0 , SPEED)
 	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var c = get_slide_collision(i)
+		if c.get_collider() is RigidBody2D:
+			c.get_collider().apply_central_impulse(-c.get_normal() * PUSH_FORCE)
